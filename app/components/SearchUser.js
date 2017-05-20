36a94daee633dfa74ed55b8b4ac90bcb6ae41f43
marpriori <marcimpriori@gmail.com>
2017-05-19 22:23:40 -0300

    Consumindo api github com axios

---
+++ app/components/SearchUser.js
@@ -0,0 +1,42 @@
+var React = require('react');
+var GitHubUser = require('../services/GitHubUser');
+
+var SearchUser = React.createClass({
+    handleSubmit: function(e){
+        e.preventDefault();
+
+        GitHubUser.getByUsername(this.refs.username.value)
+            .then(function(response){
+                console.log(response);
+            });
+        GitHubUser.getReposByUsername(this.refs.username.value)
+            .then(function(response){
+                console.log(response);
+            });
+    },
+    render : function() {
+            return (
+                <div className="container">
+                    <div className="jumbotron">
+                        <h1>GitHub Info</h1>
+                        <div className="row">
+                            <form onSubmit={this.handleSubmit}>
+                                <div className="form-group">
+                                    <label>Username</label>
+                                    <input 
+                                        type="text" 
+                                        ref="username"
+                                        className="form-control" 
+                                        placeholder="Ex: marpriori" 
+                                        />
+                                </div>
+                                <button type="submit" className="btn btn-primary">Buscar</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+});
+
+module.exports = SearchUser;
